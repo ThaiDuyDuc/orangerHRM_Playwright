9a47259722a66f6ecbab5cdcf72d1b17d02ba035
Thai Duy Duc <thaiduc20022310@gmail.com>
2025-11-20 00:13:40 +0700

    Update loginData.ts

--- src/data/loginData.ts
+++ src/data/loginData.ts
@@ -6,4 +6,12 @@ export const loginData = {
   user1: { email: "Admin", password: "admin123" },
   user2: { email: "admin", password: "admin123" },
   user3: { email: "", password: "admin123" },
+  user4: { email: "   Admin   ", password: "admin123" },
+  user5: { email: "Admin", password: "" },
+  user6: { email: "", password: "" },
+  user7: { email: "NguyenvanA", password: "admin123" },
+  user8: { email: "Admin", password: "123456" },
+  user9: { email: "NguyenVanA", password: "123456" },
+  user10: { email: "   Admin", password: "admin123" },
+  user11: { email: "Admin   ", password: "admin123" },
 };
